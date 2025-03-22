2876bd4ce0a011627d94884a8207b4429d0089df
havansang <havansang090203@gmail.com>
2025-03-22 21:44:13 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CingHuTang.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
