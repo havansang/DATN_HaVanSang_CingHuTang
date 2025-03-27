0b6754b66f7f70c0fd024e90092dc7c09696837d
havansang <havansang090203@gmail.com>
2025-03-27 23:24:09 +0700

    Add frontend client V1

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,14 @@
+﻿using CingHuTang.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CingHuTang.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
